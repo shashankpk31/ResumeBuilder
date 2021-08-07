4d22a5cd9e75b06f264b6f62753070b77b527c21
shashankpk31 <shashankpk34@gmail.com>
2021-08-07 13:33:27 +0530

    Sucessfully implemented Authenticaion

--- Resume input.php
+++ Resume input.php
@@ -1,34 +1,64 @@
+
 <?php
 include("Connection.php");
 ?>
-<!DOCTYPE html>
+
+<?php
+session_start();
+if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
+  header("location: Login.php");
+}
+?>
+
+
+<!doctype html>
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="stylesheet" href="./ResumeBuilder.css">
   <link rel="icon" href="./Asset/Logo.png" type="image/x-icon">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
   <title>Resume Builder</title>
 </head>
 
 <body>
-  <nav>
-    <div class="logo">
-      ResumeMd
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">Resume Builder</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="http://localhost/ResumeBuilder/welcome.php">Home </a>
+        </li>
+        <li class="nav-item ">
+          <a class="nav-link" href="http://localhost/ResumeBuilder/Resume%20input.php">ResumeBuild</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="http://localhost/ResumeBuilder/Resume%20view.php">Resume view</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="logout.php">Logout</a>
+        </li>
+
+      </ul>
+
+      <div class="navbar-collapse collapse">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#"> <img src="https://img.icons8.com/metro/26/000000/guest-male.png"> <?php echo "Welcome " . $_SESSION['username'] ?></a>
+          </li>
+        </ul>
+      </div>
+
+
     </div>
-    <input type="checkbox" id="click">
-    <label for="click" class="menu-btn">
-      <i class="fas fa-bars"></i>
-    </label>
-    <ul>
-      <li><a class="active" href="#">Home</a></li>
-      <li><a href="#">About</a></li>
-      <li><a href="#">Services</a></li>
-      <li><a href="#">Gallery</a></li>
-      <li><a href="#">Feedback</a></li>
-    </ul>
   </nav>
   <div class="content">
     <div class="head">
@@ -38,7 +68,7 @@ include("Connection.php");
       <fieldset>
         <legend>Fill The Form To create </legend>
         <form method="POST" enctype="multipart/form-data">
-          <label for="name" >Name</label>
+          <label for="name">Name</label>
           <input type="text" name="Username" value="" placeholder="Username here" />
           <label for="Image" id="">Image</label>
           <input type="file" value="" name="Image" placeholder="User's Image here" />
@@ -98,10 +128,14 @@ include("Connection.php");
       echo "<script>alert('UnSucessFull');</script>";
     }
     header("Location: http://localhost/ResumeBuilder/Viewresume.php");
-    
-    
   }
   ?>
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 
 </html>
